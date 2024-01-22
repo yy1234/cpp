3deb2376ea21c3d2b03d15b37a5f81075e31ca3a
yy1234 <270089758@qq.com>
2024-01-22 18:26:41 +0800

    首次提交

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   
+   int a = 10;
+   cout << "a = 哈哈" << a << endl;
+   cout << "short所占的内存空间是多少" << sizeof(short) << endl;
+    return 0;
+}
