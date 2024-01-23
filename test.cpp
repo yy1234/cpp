04ec83362f616448bd1465f7019cacc62bb4c9bd
yy1234 <270089758@qq.com>
2024-01-23 18:18:42 +0800

    测试提交

--- test.cpp
+++ test.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
 int main()
 {
-   
-   int a = 10;
-   cout << "a = 哈哈" << a << endl;
-   cout << "short所占的内存空间是多少" << sizeof(short) << endl;
+
+    int a = 10;
+    cout << "a = 哈哈\thellworld" << a << endl;
+    cout << "a = 哈哈\thellworld" << a << endl;
+    cout << "a = 哈哈\thellworld" << a << endl;
+    cout << "short所占的内存空间是多少" << sizeof(short) << endl;
+    string str = "hello world";
+    cout << str << endl;
     return 0;
 }
